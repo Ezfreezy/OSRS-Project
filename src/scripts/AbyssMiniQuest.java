0bb74e46d549357cca479b1476317e4f8c5dce2e
Ezfreezy <Jamiegibbbons@hotmail.co.uk>
2016-11-23 19:55:47 +0000

    test

--- src/scripts/AbyssMiniQuest.java
+++ src/scripts/AbyssMiniQuest.java
@@ -1,5 +1,14 @@
 package scripts;
 
-public class AbyssMiniQuest {
+import org.tribot.script.Script;
+
+public class AbyssMiniQuest extends Script {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
